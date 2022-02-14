cb739d6c073a61f3b1ea1c0b13a5a7b482d5264c
stay <58818841+stay-js@users.noreply.github.com>
2022-02-14 20:16:37 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,10 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
         <link rel="icon" href="/favicon.ico" />
         <link
           rel="stylesheet"
