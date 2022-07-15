8f64a8516a6b0ba5922ca99db1ab1665cc50e2f1
stay <nagyzeteny6@gmail.com>
2022-07-15 22:40:49 +0200

    form status

--- src/components/Form/Status/Success.tsx
+++ src/components/Form/Status/Success.tsx
@@ -1,14 +1,14 @@
 import style from './Status.module.scss';
 
 const FormSuccess: React.FC = () => (
-  <div className="text-black">
+  <div>
     <div className={style.success}>
       <svg className={style.checkmark} viewBox="0 0 52 52">
         <circle className={style.circle} cx="26" cy="26" r="25" fill="none" />
         <path className={style.check} fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" />
       </svg>
     </div>
-    <p className="text-center">Message sent successfully!</p>
+    <p className="text-center text-black">Message sent successfully!</p>
   </div>
 );
 
