14c9a6d4d134db96eb9ab616650dfc55c13d85da
Zétény Nagy <nagyzeteny6@gmail.com>
2022-07-16 08:25:14 +0200

    Update Loading.tsx

--- src/components/Form/Status/Loading.tsx
+++ src/components/Form/Status/Loading.tsx
@@ -2,7 +2,7 @@ import { Loader } from '@mantine/core';
 
 const FormLoading: React.FC = () => (
   <div className="flex flex-col items-center gap-2 mt-4 text-black">
-    <Loader className="mx-auto" />
+    <Loader />
     Please wait, processing...
   </div>
 );
