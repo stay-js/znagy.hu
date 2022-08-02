18cea5c7137f4d2d2f8510ab74cc4f41bf4fb351
stay <nagyzeteny6@gmail.com>
2022-08-02 20:21:02 +0200

    latest packages

--- src/components/Form/Status/Error.tsx
+++ src/components/Form/Status/Error.tsx
@@ -1,8 +1,8 @@
-import { AlertCircle } from 'tabler-icons-react';
+import { TbAlertCircle } from 'react-icons/tb';
 
 const FormError: React.FC = () => (
   <div className="flex flex-col items-center gap-2 mt-4">
-    <AlertCircle size={48} color="red" className="animate-bounce" />
+    <TbAlertCircle size={48} color="red" className="animate-bounce" />
     Somethnig went wrong, Please try again later!
   </div>
 );
