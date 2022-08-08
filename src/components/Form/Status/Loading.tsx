dc3cd1392c5f54b4b50b828e35799cf3711a4baa
Nagy Zétény <nagyzeteny6@gmail.com>
2022-08-08 19:22:37 +0200

    Loader color

--- src/components/Form/Status/Loading.tsx
+++ src/components/Form/Status/Loading.tsx
@@ -2,7 +2,7 @@ import { Loader } from '@mantine/core';
 
 const FormLoading: React.FC = () => (
   <div className="flex flex-col items-center gap-2 mt-4">
-    <Loader />
+    <Loader color="green" />
     Please wait, processing...
   </div>
 );
