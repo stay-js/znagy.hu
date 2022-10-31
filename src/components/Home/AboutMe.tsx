ace7cf2505e2f70fe09647a49ccfb4daea755006
Nagy Zétény <nagyzeteny6@gmail.com>
2022-10-31 20:34:51 +0100

    AboutMe Anchor component

--- src/components/Home/AboutMe.tsx
+++ src/components/Home/AboutMe.tsx
@@ -2,6 +2,12 @@ import Image from 'next/image';
 import Link from 'next/link';
 import SectionTitle from './SectionTitle';
 
+const Anchor: React.FC<{ children: React.ReactNode; href: string }> = ({ children, href }) => (
+  <Link className="link text-green-500" href={href} target="_blank" rel="noopener noreferrer">
+    {children}
+  </Link>
+);
+
 const AboutMe: React.FC = () => (
   <section className="content relative flex flex-col gap-4" id="about">
     <SectionTitle title="About Me" number={1} />
@@ -21,15 +27,7 @@ const AboutMe: React.FC = () => (
         <p>Glad you are here! </p>
         <p>
           My name is Zétény Nagy AKA stay, I&rsquo;m from Budapest, Hungary. Currently studying in{' '}
-          <Link
-            className="link text-green-500"
-            href="http://njszg.hu/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            BMSzC Neumann János Informatikai Technikum
-          </Link>
-          .
+          <Anchor href="http://njszg.hu/">BMSzC Neumann János Informatikai Technikum</Anchor>.
         </p>
         <p>
           My intrest in web development started long ago, back in my early elementary school days.
@@ -38,81 +36,30 @@ const AboutMe: React.FC = () => (
           want to be a Full Stack web developer...
         </p>
         <p>
-          If you are interested check out my{' '}
-          <Link
-            className="link text-green-500"
-            href="/setup"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            setup
-          </Link>
-          .
+          If you are interested check out my <Anchor href="/setup">setup</Anchor>.
         </p>
         <p>Favourite technologies/things I use:</p>
         <div className="ml-8 flex gap-20">
           <ul className="list-disc text-green-500">
             <li>
-              <Link
-                className="link text-green-500"
-                href="https://www.typescriptlang.org/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                TypeScript
-              </Link>
+              <Anchor href="https://www.typescriptlang.org/">TypeScript</Anchor>
             </li>
             <li>
-              <Link
-                className="link text-green-500"
-                href="https://nextjs.org/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Next.js
-              </Link>
+              <Anchor href="https://nextjs.org/">Next.js</Anchor>
             </li>
             <li>
-              <Link
-                className="link text-green-500"
-                href="https://trpc.io/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                tRPC
-              </Link>
+              <Anchor href="https://trpc.io/">tRPC</Anchor>
             </li>
           </ul>
           <ul className="list-disc text-green-500">
             <li>
-              <Link
-                className="link text-green-500"
-                href="https://www.prisma.io/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Prisma
-              </Link>
+              <Anchor href="https://www.prisma.io/">Prisma</Anchor>
             </li>
             <li>
-              <Link
-                className="link text-green-500"
-                href="https://tailwindcss.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Tailwind CSS
-              </Link>
+              <Anchor href="https://tailwindcss.com/">Tailwind CSS</Anchor>
             </li>
             <li>
-              <Link
-                className="link text-green-500"
-                href="https://vercel.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Vercel
-              </Link>
+              <Anchor href="https://vercel.com/">Vercel</Anchor>
             </li>
           </ul>
         </div>
