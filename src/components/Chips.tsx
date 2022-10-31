00a3e5860d1a0053d747e605d2513812ea8c1f74
Nagy Zétény <nagyzeteny6@gmail.com>
2022-10-31 20:07:32 +0100

    Update chip colors

--- src/components/Chips.tsx
+++ src/components/Chips.tsx
@@ -78,7 +78,7 @@ const Chips: React.FC<{ variant: string }> = ({ variant }) => {
 
   if (variant === 'Tailwind CSS')
     return (
-      <Chip color="#338bdf9" href="https://tailwindcss.com/">
+      <Chip color="##38b2ac" href="https://tailwindcss.com/">
         Tailwind CSS
       </Chip>
     );
