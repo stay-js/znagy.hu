02104a30074cb8240202210471a97d77dce9d580
Nagy Zétény <nagyzeteny6@gmail.com>
2022-11-04 20:01:35 +0100

    Update Title.tsx

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,6 +1,6 @@
 const Title: React.FC<{ title: string; children: React.ReactNode }> = ({ title, children }) => (
-  <div className="flex flex-col items-center gap-4 text-center">
-    <h1 className="bg-gradient-to-r from-teal-400 to-green-500 bg-clip-text text-5xl font-bold text-transparent">
+  <div className="flex flex-col items-center gap-2 text-center">
+    <h1 className="bg-gradient-to-r from-teal-400 to-green-500 bg-clip-text text-4xl font-bold text-transparent">
       {title}
     </h1>
     <div className="max-w-prose">{children}</div>
