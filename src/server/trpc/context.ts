ed3a9b6f5bfa7259e6840648035745eb48776266
Nagy Zétény <nagyzeteny6@gmail.com>
2022-11-27 11:11:40 +0100

    Update context.tsx

--- src/server/trpc/context.ts
+++ src/server/trpc/context.ts
@@ -1,7 +1,13 @@
 import type { inferAsyncReturnType } from '@trpc/server';
+import type { CreateNextContextOptions } from '@trpc/server/adapters/next';
 
-export const createContext = async () => {
-  return {};
+export const createContext = async (opts: CreateNextContextOptions) => {
+  const { req, res } = opts;
+
+  return {
+    req,
+    res,
+  };
 };
 
 export type Context = inferAsyncReturnType<typeof createContext>;
