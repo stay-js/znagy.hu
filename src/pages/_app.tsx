5e0f68a04619887bac5e5ae5a4c5c63d898a3d4d
Nagy Zétény <nagyzeteny6@gmail.com>
2023-02-25 20:28:54 +0100

    removed ts-reset

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,3 @@
-import '@total-typescript/ts-reset';
-
 import type { AppType } from 'next/dist/shared/lib/utils';
 import { useRouter } from 'next/router';
 import { Navigation } from '@components/Navigation';
