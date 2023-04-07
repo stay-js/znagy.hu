85ccc8c148dc6acf685fd2edd2d50b1a002c3e34
Nagy Zétény <nagyzeteny6@gmail.com>
2023-04-07 18:34:20 +0200

    vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,23 @@
-import type { AppType } from 'next/dist/shared/lib/utils';
-import { useRouter } from 'next/router';
 import { Navigation } from '@components/Navigation';
 import { ScrollTop } from '@components/ScrollTop';
 import { trpc } from '@utils/trpc';
+import { Analytics } from '@vercel/analytics/react';
+import type { AppType } from 'next/dist/shared/lib/utils';
+import { useRouter } from 'next/router';
 
 import '@styles/globals.css';
 
-const App: AppType = ({ Component, pageProps }) => (
-  <>
-    <ScrollTop />
-    <Navigation route={useRouter().asPath} />
-    <Component {...pageProps} />
-  </>
-);
+const App: AppType = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  return (
+    <>
+      <Analytics />
+      <ScrollTop />
+      <Navigation route={router.asPath} />
+      <Component {...pageProps} />
+    </>
+  );
+};
 
 export default trpc.withTRPC(App);
