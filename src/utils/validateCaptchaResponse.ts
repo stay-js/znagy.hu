4e1dde11249dce750b63fce0809980aebdf8bf07
Nagy Zétény <nagyzeteny6@gmail.com>
2023-05-01 14:42:26 +0200

    Update validateCaptchaResponse.ts

--- src/utils/validateCaptchaResponse.ts
+++ src/utils/validateCaptchaResponse.ts
@@ -1,8 +1,6 @@
 import { env } from 'src/env.mjs';
 
-export const validateCaptchaResponse = async (token?: string | null) => {
-  if (!token) return false;
-
+export const validateCaptchaResponse = async (token: string) => {
   const url = new URL('https://www.google.com/recaptcha/api/siteverify');
   url.searchParams.set('secret', env.RECAPTCHA_SECRET_KEY);
   url.searchParams.set('response', token);
