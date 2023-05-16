acca082688736fe3a8025410df484e17295a85d0
Nagy Zétény <nagyzeteny6@gmail.com>
2023-05-16 17:47:01 +0200

    fix anchor component

--- src/components/Anchor.tsx
+++ src/components/Anchor.tsx
@@ -6,7 +6,7 @@ export const Anchor: React.FC<{
   children: React.ReactNode;
 }> = ({ href, target, className = '', children }) => (
   <Link
-    className={`bg-gradient-to-r from-green-400 to-green-700 bg-[length:0%_2px] bg-left-bottom bg-no-repeat transition duration-200 hover:bg-[length:100%_2px] ${className}`}
+    className={`bg-gradient-to-r from-green-400 to-green-700 bg-[length:0%_2px] bg-left-bottom bg-no-repeat transition-all duration-200 hover:bg-[length:100%_2px] ${className}`}
     href={href}
     target={target}
     {...(target === '_blank' && { rel: 'noopener noreferrer' })}
