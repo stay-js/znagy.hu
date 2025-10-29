a100d677f4e591fa368e71bdd77ae8d3bdabc6a8
Nagy Zétény <nagyzeteny6@gmail.com>
2025-10-29 12:08:29 +0100

    add metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,14 @@ import { Projects } from '~/components/projects';
 import { Skills } from '~/components/skills';
 import { Experience } from '~/components/expreience';
 import { Contact } from '~/components/contact';
+import { createMetadata } from '~/lib/create-metadata';
+
+export const metadata = createMetadata({
+  path: '/',
+  title: 'Home',
+  description:
+    'I build digital experiences with modern technologies and thoughtful engineering. Currently studying at BMSzC Neumann János Informatikai Technikum in Budapest, Hungary.',
+});
 
 const Page: React.FC = () => (
   <>
---
+++ src/lib/create-metadata.ts
@@ -0,0 +1,57 @@
+import type { Metadata } from 'next';
+import { keywords } from '~/constants/keywords';
+
+export const createMetadata = ({
+  path,
+  title,
+  absoluteTitle,
+  description,
+}: {
+  path: string;
+  title: string;
+  absoluteTitle?: string;
+  description: string;
+}): Metadata => ({
+  metadataBase: new URL('https://znagy.hu'),
+
+  authors: [{ name: 'Zétény Nagy', url: 'https://znagy.hu' }],
+  creator: 'Zétény Nagy',
+
+  keywords: keywords.join(', '),
+
+  title: absoluteTitle ?? `${title} - Zétény Nagy`,
+  description,
+
+  applicationName: 'Zétény Nagy',
+
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+
+  openGraph: {
+    type: 'website',
+    url: `https://znagy.hu${path}`,
+    title: absoluteTitle ?? `${title} - Zétény Nagy`,
+    description,
+    siteName: 'Zétény Nagy',
+    locale: 'en-US',
+  },
+
+  twitter: {
+    card: 'summary',
+    title: absoluteTitle ?? `${title} - Zétény Nagy`,
+    description,
+  },
+
+  icons: {
+    icon: '/favicon.ico',
+  },
+});
