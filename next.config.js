af7e27145124a26e9a5757abb6d733f032be2223
Nagy Zétény <nagyzeteny6@gmail.com>
2025-10-29 12:21:37 +0100

    add resume redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 import './src/env.js';
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  redirects() {
+    return [
+      {
+        source: '/resume',
+        destination: '/#experience',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default config;
