fb69dfe90e7a6e1ee36e2f3d607921efd5fcb568
Nagy Zétény <nagyzeteny6@gmail.com>
2026-04-07 12:52:29 +0200

    feat: move form reset to submit handler

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback, useRef } from 'react';
 import ReCAPTCHA from 'react-google-recaptcha';
 import { useForm } from 'react-hook-form';
 import { toast } from 'sonner';
@@ -34,14 +34,14 @@ export function Contact() {
 
   const {
     control,
-    formState: { isSubmitSuccessful, isSubmitting },
+    formState: { isSubmitting },
     handleSubmit,
     reset,
   } = useForm<FormSchema>({ defaultValues, resolver: zodResolver(formSchema) });
 
   const handleFormSubmit = useCallback(
     (e: React.SubmitEvent<HTMLFormElement>) => {
-      void handleSubmit(async (data) => {
+      async function onSubmit(data: FormSchema) {
         const token = await captchaRef.current?.executeAsync().catch(() => null);
         captchaRef.current?.reset();
 
@@ -49,19 +49,18 @@ export function Contact() {
 
         try {
           await sendEmail({ ...data, token });
-          toast.success('Message sent successfully! I will get back to you as soon as possible.');
-        } catch (err) {
-          toast.error('Something went wrong. Please try again later.');
-          throw err;
-        }
-      })(e);
-    },
-    [handleSubmit],
-  );
 
-  useEffect(() => {
-    if (isSubmitSuccessful) reset(defaultValues);
-  }, [isSubmitSuccessful, reset]);
+          toast.success('Message sent successfully! I will get back to you as soon as possible.');
+          reset(defaultValues);
+        } catch {
+          toast.error('Something went wrong. Please try again later.');
+        }
+      }
+
+      void handleSubmit(onSubmit)(e);
+    },
+    [handleSubmit, reset],
+  );
 
   return (
     <section className="dark:bg-muted/30 bg-input/30 scroll-m-8 py-24" id="contact">
